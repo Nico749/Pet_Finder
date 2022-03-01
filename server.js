dc727737014942620be150b181e84fd70ec51cb7
Ewa Kubik <ewa.kubik@hotmail.com>
2022-03-01 20:58:01 +1100

    basic routes setup

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const htmlRouter = require('./routes/htmlRoutes');
+const apiRouter = require("./routes/apiRoutes");
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -34,7 +36,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+
+// Routes directory
+app.use('/', htmlRouter);
+app.use('/api', apiRouter);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
