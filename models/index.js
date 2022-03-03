5f5f95739feb733110551f22d89405834703033b
Erin Hatherell <93974950+emhat1@users.noreply.github.com>
2022-03-03 19:13:35 +1100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,17 +1,19 @@
 const Pet = require('./Pet');
 const User = require('./User');
 
-// Define an Pet as having one User to create a foreign key in the `Pet` table
 Pet.hasOne(User, {
   foreignKey: 'user_id',
   // When we delete a User, make sure to also delete the associated Pet
-  onDelete: 'CASCADE',
 });
 
-// We can also define the association starting with Pet
 Pet.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-// We package our two models and export them as an object so we can import them together and use their proper names
+User.hasMany(Pet, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+
 module.exports = { Pet, User };
