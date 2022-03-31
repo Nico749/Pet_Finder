a7c8f6fd7d6802334ce4cda3d23dc4c19e99b1e2
Nico Pasqualini <pasqualinin9@gmail.com>
2022-03-31 21:33:06 +1100

    changing style

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "public")));
 
