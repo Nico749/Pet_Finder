1bb10ddbfd8072362e18472444e02e6edb6a5b24
Nico Pasqualini <pasqualinin9@gmail.com>
2022-04-21 11:42:13 +1000

    setting bg

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
